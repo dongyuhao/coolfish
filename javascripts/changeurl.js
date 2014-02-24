50bc5af36b8a60c4f0ce447cd2fea6657d48be25
dongyuhao <dyh14@sina.com>
2014-02-24 10:22:05 +0800

    导航逻辑优化

--- javascripts/changeurl.js
+++ javascripts/changeurl.js
@@ -1,20 +1,4 @@
 
-
-
-
-function changeit(url){
-  aa.src=url;
-}
-function changeimage(url)
-{
-	document.getElementById("iframepage").src = "imge.html"
-}
-
-function changeabout(url)
-{
-	document.getElementById("iframepage").src = "about.html"
-}
-
 function iFrameHeight() { 
 	var ifm= document.getElementById("iframepage"); 
 	var subWeb = document.frames ? document.frames["iframepage"].document : ifm.contentDocument; 
